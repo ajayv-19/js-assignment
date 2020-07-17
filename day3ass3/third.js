a680b5dfadccce01304e9bae40e4c2af5e1098bb
ajayv-19 <68264798+ajayv-19@users.noreply.github.com>
2020-07-17 13:35:19 +0530

    Add files via upload

---
+++ day3ass3/third.js
@@ -0,0 +1,16 @@
+console.log(" day 3 asssinment 3"
+)
+let marks=Number(prompt("Enter the marks:",0));
+if (marks >80)
+    {
+        console.log(`Marks are ${marks} and the grade is A`);
+    }
+else if(marks >=50 && marks<=80)
+{
+   
+    console.log(`Marks are ${marks} and the grade is B`); 
+}
+else{
+    
+    console.log(`Marks are ${marks} and the grade is c`);
+}
