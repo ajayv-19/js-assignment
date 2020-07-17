a073766d32f96e7876e98db68595ad30b614accd
ajayv-19 <68264798+ajayv-19@users.noreply.github.com>
2020-07-17 13:31:06 +0530

    Add files via upload

---
+++ day3ass1/first.js
@@ -0,0 +1,10 @@
+let number=Number(prompt("enter the number",0))
+//console.log(number)
+if (number%2==0)
+{
+    console.log(`The number entered is ${number} and the number is even`);
+}
+else
+{
+console.log(`The number entered is ${number} and the number is odd`);
+}
