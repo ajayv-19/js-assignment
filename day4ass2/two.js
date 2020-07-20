ccde1b5f12facdc065bad3ebbd4351a5eda9f68d
ajayv-19 <68264798+ajayv-19@users.noreply.github.com>
2020-07-20 13:38:06 +0530

    Add files via upload

---
+++ day4ass2/two.js
@@ -0,0 +1,9 @@
+const student={
+    name:"Helsinki",
+    age:24,
+    projects:{
+        diceGame:"Two player dice game using Javascript"
+    }
+}
+const{name,age,projects:{diceGame}}=student;
+console.log(name,age,diceGame)
