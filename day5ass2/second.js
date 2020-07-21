18632a664029fe378423ed8967d3ea90e35a9444
ajayv-19 <68264798+ajayv-19@users.noreply.github.com>
2020-07-21 14:05:03 +0530

    Add files via upload

---
+++ day5ass2/second.js
@@ -0,0 +1,68 @@
+class User {
+    constructor(name, age,email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+      this.luCoins = 0;
+      this.courses = [];
+    }
+
+    static greet(){
+        console.log("Hello There");
+    }
+
+    login(){
+        console.log(`${this.name} has logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.name} has logged out`);
+        return this;
+    }
+    addCoins(){
+        this.luCoins++;
+        console.log(`${this.name} has ${this.luCoins} coins`);
+        return this;
+    }
+    getDetails(){
+        console.log(`Name is ${this.name}, email is ${this.email}`);
+        return this;
+    }
+
+}
+
+class Moderator extends User{
+    constructor(name,age,email,role){
+        super(name,age,email);
+        this.role = role;
+    }
+
+    deleteUser(user){
+        users = users.filter(u =>{
+            return u.email != user.email; 
+        })
+    }
+
+}
+
+class Admin extends Moderator{
+   addCourse(user,course){
+       user.courses.push(course);
+       console.log(user);
+   }
+}
+
+let user1 = new User('Ajay',21,'ajay@gmail.com')
+let user2 = new User('Vinay',20,'viay@gmai.com')
+let mod = new Moderator('murty',19,'murthy@gmail.com','Moderator');
+let admin = new Admin('Rio',25,'r@gmail.com');
+let users = [user1,user2,mod,admin];
+
+users.forEach(element => {
+    console.log(element);
+});
+
+User.greet();
+admin.addCourse(user1,'Javascript');
+admin.addCourse(user1,'Python');
+
